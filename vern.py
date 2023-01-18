38799c90b313715488324a6aff08df3e5ac0c05b
Valik <91689953+chpoknut@users.noreply.github.com>
2023-01-18 17:13:31 +0200

    Create vern.py

---
+++ vern.py
@@ -0,0 +1,102 @@
+def func1(arg1, arg2):
+    var35 = func2(arg1, arg2)
+    var38 = class4()
+    for var39 in xrange(6):
+        var40 = var38.func5
+        var40(arg1, var39)
+    var46 = func6(arg1, arg2)
+    var65 = func7(arg2, var35)
+    var66 = (arg2 + var65) - 945272268
+    if arg1 < var65:
+        var67 = var46 & (var66 & arg1 + var35)
+    else:
+        var67 = arg2 + var35 | (var46 & arg2)
+    var68 = (var66 ^ var46) - ((575 & var35 + (arg1 + (-190 + ((var66 & 764 + var66) ^ -625) ^ arg1) | ((var65 ^ var65) & var35))) - (var66 & (arg2 & (-21 | var66 - var66 & arg1)) | arg1)) - var65
+    result = var66 | arg2
+    return result
+def func7(arg47, arg48):
+    var49 = -355559671 | -1865667421
+    var50 = var49 + var49 ^ 1689658225
+    var51 = var49 ^ -1387291472
+    if var51 < var50:
+        var52 = (1963787378 | 276 - var50) | arg48
+    else:
+        var52 = arg48 ^ (arg48 + var50)
+    if var49 < var49:
+        var53 = var49 & var50 | arg47 + var51
+    else:
+        var53 = var50 & arg48 & 731 - -471804281
+    var54 = 140845440 & var50
+    var55 = var54 + -86303487
+    var56 = var50 ^ var54 + arg47 & arg47
+    var57 = var55 & var56
+    var58 = var50 - var57 & -841 ^ var51
+    if var54 < var56:
+        var59 = (arg48 + (arg47 + arg48)) - -371
+    else:
+        var59 = arg48 + var55 & var50 - arg48
+    var60 = var57 & (-334 & arg47)
+    var61 = var56 & var60
+    var62 = var49 & var54
+    if var60 < var61:
+        var63 = var57 + (var49 & (var50 & arg48))
+    else:
+        var63 = var51 | 630062425
+    var64 = arg48 - var49 & (var61 ^ var54)
+    result = var51 & arg48
+    return result
+def func6(arg41, arg42):
+    var43 = (-1412429381 + arg42) - (1394124943 | -507 ^ 627711133)
+    var44 = (((-140556400 ^ 1465613692) & (-978 | arg42 - 212) & var43) + var43) + -275
+    var45 = -406813470 & ((var43 & var44 ^ -2132924982) | 597547046)
+    result = var43 - 417791813 + 860
+    return result
+class class4(object):
+    def func5(self, arg36, arg37):
+        result = (-162060109 ^ (arg36 + arg36)) + arg37
+        return result
+def func2(arg3, arg4):
+    var9 = func3(arg4, arg3)
+    var10 = arg3 ^ arg3 ^ (arg4 & arg4)
+    var11 = ((var10 - var9) | var9) & -1956298407
+    var12 = 502 & (arg3 & var9) & -556
+    var13 = var12 - -862584700 ^ var12
+    if var12 < var12:
+        var14 = var10 - var12
+    else:
+        var14 = var9 - var11
+    var15 = var13 - var9
+    var16 = var12 - var11
+    var17 = var15 + var10 + var12 | var16
+    var18 = (var9 | var15) & var13 - arg3
+    var19 = (var12 - arg4) | var11
+    var20 = ((var13 ^ var16) - var19) | 990
+    var21 = var15 - var18
+    var22 = (var11 & var20) ^ var9 | var19
+    var23 = (-835 & var21) + var17 | var10
+    var24 = (var13 ^ var22 & var20) ^ var21
+    var25 = (var10 ^ var12 - var10) + var18
+    var26 = var15 ^ (var25 ^ var12 ^ var23)
+    var27 = -2 | var13 ^ arg3 + var16
+    var28 = var17 - ((var15 & arg4) - var10)
+    var29 = (var17 + var19) & var23 + var26
+    var30 = (var20 | var12 ^ var29) + var9
+    var31 = arg4 - var22 + (var16 - var30)
+    var32 = (var11 & var27) + var22
+    var33 = var32 - ((var18 - var18) ^ var29)
+    var34 = (var31 ^ var19 + arg4) + var17
+    result = var12 - var33 ^ var18
+    return result
+def func3(arg5, arg6):
+    var7 = 0
+    for var8 in range(26):
+        var7 += var8 | (var8 | -2)
+    return var7
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 8'
+    print 'arg_number: 69'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
